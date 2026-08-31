6187010a3c2ca4c24b71073188c2462e3a52c1b0
SemSreyleak11111111111111 <kanhaleak777@gmail.com>
2026-08-31 17:23:04 +0700

    lower space ml 05 at card jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,13 +12,13 @@ function ProductCard({ product }) {
       </div>
 
       <div className="mt-4">
-        <p className="text-sm uppercase tracking-widest text-zinc-500">CODE :
-          <span className="ml-1"> {product.name}</span>
+        <p className="text-sm uppercase tracking-widest text-zinc-500">
+          CODE:<span className="ml-0.5">{product.name}</span>
         </p>
         <h3 className="mt-1 text-sm font-medium">
           Size : <span className="ml-1">{product.size}</span>
         </h3>
-       
+
         <p className="mt-2 text-sm text-zinc-400">
           {product.both_price}
         </p>
